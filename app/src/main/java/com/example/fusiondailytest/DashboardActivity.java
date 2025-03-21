438a901dc4328e9d13d27c04eb7f1c10db7be113
Benny <melodiouscoast7@gmail.com>
2025-03-20 18:07:34 -0600

    Finished first version of Dashboard UI, mostly nonfunctional

---
+++ app/src/main/java/com/example/fusiondailytest/DashboardActivity.java
@@ -0,0 +1,28 @@
+package com.example.fusiondailytest;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.fusiondailytest.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
